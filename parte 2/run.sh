4348fe325a3c8f640392e169228c872e775003db
Fabian-Gobet <fabian.mas.slax@gmail.com>
2023-05-28 11:11:13 +0100

    part2

---
+++ parte 2/run.sh
@@ -0,0 +1,4 @@
+mongod -f cfg0.conf
+mongod -f a0.conf
+mongod -f b0.conf
+mongod -f c0.conf
---
+++ parte 2/run0.sh
@@ -0,0 +1,4 @@
+/mongo/mongod -f cfg0.conf
+/mongo/mongod -f a0.conf
+/mongo/mongod -f b0.conf
+/mongo/mongod -f c0.conf
